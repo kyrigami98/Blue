eb9d53d331a01b15a1de1a0e1c0a6ea34149e6bf
kyrigami98 <31952492+kyrigami98@users.noreply.github.com>
2019-12-11 21:24:51 +0100

    first

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php
+include "INCLUSION/header.php";
+
+include "INCLUSION/Login.php";
+
+include "INCLUSION/footer.php";
+?>
---
+++ profil.php
@@ -0,0 +1,169 @@
+
+<?php
+include "INCLUSION/header.php";
+?>
+
+<div class="container">
+  <div class="card border-0 shadow my-5">
+    <div class="card-body p-5">
+
+
+  <form method="post">
+
+      <div class="row">
+                    <div class="col-md-4">
+                        <div class="profile-img">
+							<h4>Kyrigami</h4><h6 class="text-muted">Dessinateur</h6>
+						
+                            <img class="avatar rounded img-fluid" src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png"/>
+                            <div class="file btn btn-lg btn-primary">
+                                Change Photo
+                                <input type="file" name="file"  class="file-upload"/>
+                            </div>
+							
+							
+                        </div>
+							
+						<div class="row">
+							<div class="col-md-12">
+								<div class="profile-work">
+								  <ul class="list-group">
+									<li class="list-group-item text-muted">Activité <i class="fa fa-dashboard fa-1x"></i></li>
+									<li class="list-group-item text-right"><span class="pull-left"><strong>Partage</strong></span> 125</li>
+									<li class="list-group-item text-right"><span class="pull-left"><strong>Likes</strong></span> 13</li>
+									<li class="list-group-item text-right"><span class="pull-left"><strong>Nombre de Projet</strong></span> 2</li>
+									<li class="list-group-item text-right"><span class="pull-left"><strong>Followers</strong></span> 78</li>
+								  </ul> 
+									
+								</div>
+							</div>
+						
+						</div>
+									
+						  <br>
+                    </div>
+					
+                    <div class="col-md-6">
+                        <div class="profile-head">
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Information</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Projets</a>
+                                </li>
+                            </ul>
+                        </div>
+							
+                    <div class="col-md-12">
+                        <div class="tab-content profile-tab" id="myTabContent">
+						
+						    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                               <br>  
+							   
+								<div class="col-lg-8 col-sm-8 mb-4">
+								  <div class="card h-100">
+									<a href="#"><img class="card-img-top" src="IMAGES/giphy1.gif" alt=""></a>
+									 <div class="card-body">
+									  <h4 class="card-title">
+										<a href="#">Symfonia</a>
+									  </h4>
+									</div>
+								  </div>
+								</div>
+								
+								<div class="col-lg-8 col-sm-8 mb-4">
+								  <div class="card h-100">
+									<a href="#"><img class="card-img-top" src="IMAGES/dragon-ball-z.jpg" alt=""></a>
+									 <div class="card-body">
+									  <h4 class="card-title">
+										<a href="#">Badass</a>
+									  </h4>
+									</div>
+								  </div>
+								</div>
+								
+                            </div>
+						
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+								   
+							 
+									<hr>
+									  <div class="form-label-group">
+										<div class="form-group">
+										  <label for="Titre">Titre</label>
+										  <select class="form-control" id="Titre">
+											<option>Déssinateur</option>
+											<option>Sénariste</option>
+											<option>Déssinateur & Sénariste</option>
+										  </select>
+										</div>
+									  </div>
+					  
+									<hr>
+							   
+									<div class="form-label-group">
+										<input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus>
+										<label for="inputUserame">Pseudo</label>
+									  </div>
+									  
+									<div class="form-label-group">
+										<input type="email" id="inputEmail2" class="form-control" placeholder="Email address" required>
+										<label for="inputEmail2">Email</label>
+									  </div>
+									 
+									 <hr>
+
+									  <div class="form-label-group">
+										<input type="password" id="inputPassword2" class="form-control" placeholder="Password" required>
+										<label for="inputPassword2">Ancien Mot de passe</label>
+									  </div>
+									  
+									<div class="form-label-group">
+										<input type="password" id="inputPassword3" class="form-control" placeholder="Password" required>
+										<label for="inputPassword3">Nouveau Mot de passe</label>
+									  </div>
+											
+									 <div class="form-label-group">        
+										<div class="form-check">
+										  <label class="form-check-label">
+											<input type="checkbox" class="form-check-input" value="">J'ai lu et j'accepte les <a href="">conditions d'utilisation<a>
+										  </label>
+										</div>
+									 </div>
+									 
+									<div class="row pull-right">
+										<div class="form-group">
+											<div class="col-xs-12">
+											<br>
+											<button class="btn btn-lg btn-success pull-right" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
+											<button class="btn btn-lg  pull-left" type="reset"><i class="fa fa-repeat"></i> Reset</button>
+											</div>
+										</div>
+								</div>
+									
+                            </div>
+							
+                        
+							
+						</div>
+                       </div>
+                    </div>
+					
+            
+       </div>
+			
+            </form>           
+	
+    </div>
+
+
+    </div>
+  </div>
+</div>
+
+
+
+<?php
+include "INCLUSION/footer.php";
+?>
