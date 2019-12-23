3526d0cc455c1a87a93c9655115d919ddf1c0c65
Kyriel DOSSOU <Kyrigami@gmail.com>
2019-12-23 01:01:37 +0100

    Amelioration Frontend  +  debut de l'atelier

--- INCLUSION/header.php
+++ INCLUSION/header.php
@@ -16,67 +16,216 @@
  <link rel="stylesheet" href="CSS/all.css">
  <link rel="stylesheet" href="CSS/bootstrap.css">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
-<br><br>
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">BLUE</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-	  
-	   <li class="nav-item">
-			<input type="text" id="search" class="form-control" placeholder="Rechercher un projet..." required autofocus>
-	   </li>
-        <li class="nav-item">
-          	<button class="btn" type="submit"><i class="fa fa-search"></i></button>
-        </li>
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 fixed-top shadow">
+		<a class="navbar-brand" href="index.php">BLUE</a>
+          <!-- Sidebar Toggle (Topbar) -->
+
+          <!-- Topbar Search -->
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+              <input type="text" class="form-control bg-light border-0 small" placeholder="Rechercher un projet ou un auteur..." aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+              </a>
+              <!-- Dropdown - Messages -->
+              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                <form class="form-inline mr-auto w-100 navbar-search">
+                  <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+
+ 
 		<?php
 			if(!isset($_SESSION['pseudo']))
 			{
 		echo "<li class=\"nav-item\">
-		 <a class=\"\" href=\"#inscription\">
+		 <a class=\"nav-link\" href=\"#inscription\">
 			<button class=\"btn text-uppercase\" type=\"submit\">Inscription</button>
         </a>
+		
 		</li>
         <li class=\"nav-item\">    
+		<a class=\"nav-link\" href=\"#\">
 			<button class=\"btn btn-primary text-uppercase\" data-toggle=\"modal\" data-target=\".bd-example-modal-lg\">Connexion</button>
-        </li>";
+        </a>
+        </li>
+		";
 			}
 			else
 			{
-		echo "<li class=\"nav-item active\">
-		  
-		      <li class=\"dropdown\"> 
+		echo "
+		
+		           <!-- Nav Item - Alerts -->
+            <li class=\"nav-item dropdown no-arrow mx-1\">
+              <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"alertsDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                <i class=\"fas fa-bell fa-fw\"></i>
+                <!-- Counter - Alerts -->
+                <span class=\"badge badge-danger badge-counter\">3+</span>
+              </a>
+              <!-- Dropdown - Alerts -->
+              <div class=\"dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in\" aria-labelledby=\"alertsDropdown\">
+                <h6 class=\"dropdown-header\">
+                  Alerts Center
+                </h6>
+                <a class=\"dropdown-item d-flex align-items-center\" href=\"#\">
+                  <div class=\"mr-3\">
+                    <div class=\"icon-circle bg-primary\">
+                      <i class=\"fas fa-file-alt text-white\"></i>
+                    </div>
+                  </div>
+                  <div>
+                    <div class=\"small text-gray-500\">December 12, 2019</div>
+                    <span class=\"font-weight-bold\">Le Projet manga \"Descente Aux Enfer\" à été mise a jour!</span>
+                  </div>
+                </a>
+                <a class=\"dropdown-item d-flex align-items-center\" href=\"#\">
+                  <div class=\"mr-3\">
+                    <div class=\"icon-circle bg-success\">
+                      <i class=\"fas fa-thumbs-up text-white\"></i>
+                    </div>
+                  </div>
+                  <div>
+                    <div class=\"small text-gray-500\">December 7, 2019</div>
+                     Kriss à aimer votre projet Symfonia
+                  </div>
+                </a>
+                <a class=\"dropdown-item d-flex align-items-center\" href=\"#\">
+                  <div class=\"mr-3\">
+                    <div class=\"icon-circle bg-warning\">
+                      <i class=\"fas fa-exclamation-triangle text-white\"></i>
+                    </div>
+                  </div>
+                  <div>
+                    <div class=\"small text-gray-500\">December 2, 2019</div>
+                     Veillez confirmer votre email.
+                  </div>
+                </a>
+                <a class=\"dropdown-item text-center small text-gray-500\" href=\"#\">Show All Alerts</a>
+              </div>
+            </li>
 
-                    <a href=\"#\" class=\"nav-link dropdown-toggle\" id=\"navbarDropdown\" data-toggle=\"dropdown\" aria-expanded=\"false\"> 
-                            Bienvenue, <strong>".$_SESSION['pseudo']."</strong><b class=\"caret\"></b>
-                        </a>
+            <!-- Nav Item - Messages -->
+            <li class=\"nav-item dropdown no-arrow mx-1\">
+              <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"messagesDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                <i class=\"fas fa-envelope fa-fw\"></i>
+                <!-- Counter - Messages -->
+                <span class=\"badge badge-danger badge-counter\">7</span>
+              </a>
+              <!-- Dropdown - Messages -->
+              <div class=\"dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in\" aria-labelledby=\"messagesDropdown\">
+                <h6 class=\"dropdown-header\">
+                  Message Center
+                </h6>
+                <a class=\"dropdown-item d-flex align-items-center\" href=\"#\">
+                  <div class=\"dropdown-list-image mr-3\">
+                    <img class=\"rounded-circle\" src=\"https://source.unsplash.com/fn_BT9fwg_E/60x60\" alt=\"\">
+                    <div class=\"status-indicator bg-success\"></div>
+                  </div>
+                  <div class=\"font-weight-bold\">
+                    <div class=\"text-truncate\">Je vois que tu dessine Super bien, </div>
+                    <div class=\"small text-gray-500\">Emily Fowler · 58m</div>
+                  </div>
+                </a>
+                <a class=\"dropdown-item d-flex align-items-center\" href=\"#\">
+                  <div class=\"dropdown-list-image mr-3\">
+                    <img class=\"rounded-circle\" src=\"https://source.unsplash.com/AU4VPcFN4LE/60x60\" alt=\"\">
+                    <div class=\"status-indicator\"></div>
+                  </div>
+                  <div>
+                    <div class=\"text-truncate\">ca te dirais de travailler avec moi sur un projet?</div>
+                    <div class=\"small text-gray-500\">Jae Chun · 1d</div>
+                  </div>
+                </a>
+                <a class=\"dropdown-item d-flex align-items-center\" href=\"#\">
+                  <div class=\"dropdown-list-image mr-3\">
+                    <img class=\"rounded-circle\" src=\"https://source.unsplash.com/CS2uCrpNzJY/60x60\" alt=\"\">
+                    <div class=\"status-indicator bg-warning\"></div>
+                  </div>
+                  <div>
+                    <div class=\"text-truncate\">Last month's report looks great, I am very happy with the progress so far, keep up the good work!</div>
+                    <div class=\"small text-gray-500\">Morgan Alvarez · 2d</div>
+                  </div>
+                </a>
+                <a class=\"dropdown-item d-flex align-items-center\" href=\"#\">
+                  <div class=\"dropdown-list-image mr-3\">
+                    <img class=\"rounded-circle\" src=\"https://source.unsplash.com/Mv9hjnEUHR4/60x60\" alt=\"\">
+                    <div class=\"status-indicator bg-success\"></div>
+                  </div>
+                  <div>
+                    <div class=\"text-truncate\">Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</div>
+                    <div class=\"small text-gray-500\">Chicken the Dog · 2w</div>
+                  </div>
+                </a>
+                <a class=\"dropdown-item text-center small text-gray-500\" href=\"#\">Read More Messages</a>
+              </div>
+            </li>
 
-                        <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
-						<img class=\"card-img-top\" src=\"IMAGES/PROFILS/".$_SESSION['image']."\" alt=\"\">
-                                <a class=\"dropdown-item\" href=\"profil.php\">Mon profil</a>
-                                <a class=\"dropdown-item\" href=\"atelier.php\">Mon atelier</a>
-                                <div class=\"dropdown-divider\"></div>
-                                <a class=\"dropdown-item\" href=\"#\">
-								<a href=\"DATABASE/systeme.php\"><button class=\"btn btn-danger text-uppercase\" style=\"margin-left:5%;\">Deconnexion</button></a>
-								</a>
-                        </div>
-
-                </li> 
-		  
-                <span class=\"sr-only\">(current)</span>
-             
-        </li>";
+            <div class=\"topbar-divider d-none d-sm-block\"></div>
+		
+		
+		      <!-- Nav Item - User Information -->
+            <li class=\"nav-item dropdown no-arrow\">
+			
+              <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"userDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                <span class=\"mr-2 d-none d-lg-inline text-gray-600 small\">Bienvenue, <strong>".$_SESSION['pseudo']."</strong></span>
+                <img class=\"img-profile rounded-circle\" src=\"IMAGES/PROFILS/".$_SESSION['image']."\">
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class=\"dropdown-menu dropdown-menu-right shadow animated--grow-in\" aria-labelledby=\"userDropdown\">
+                <a class=\"dropdown-item\" href=\"profil.php\">
+                  <i class=\"fas fa-user fa-sm fa-fw mr-2 text-gray-400\"></i>
+                  Mon Profil
+                </a>
+                <a class=\"dropdown-item\" href=\"atelier.php\">
+                  <i class=\"fas fa-cogs fa-sm fa-fw mr-2 text-gray-400\"></i>
+                  Mon atelier
+                </a>
+                <div class=\"dropdown-divider\"></div>
+                <a class=\"dropdown-item\" href=\"DATABASE/systeme.php\">
+                  <i class=\"fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400\"></i>
+                  Deconnexion
+                </a>
+              </div>
+            </li>
+			";
 			}?>
-      </ul>
-    </div>
-  </div>
-</nav>
 
+          </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+
+<br><br>
 <body>
--- atelier.php
+++ atelier.php
@@ -3,14 +3,260 @@
 include "INCLUSION/header.php";
 ?>
 
-<div class="container">
+<div class="container-fluid">
   <div class="card border-0 shadow my-5">
-    <div class="card-body p-5">
-      <h1 class="font-weight-light">Atelier</h1>
-      <p class="lead">In this snippet, the background image is fixed to the body element. Content on the page will scroll, but the image will remain in a fixed position!</p>
-      <p class="lead">Scroll down...</p>
-      <div style="height: 700px"></div>
-      <p class="lead mb-0">You've reached the end!</p>
+    <div class="card-body">
+	  
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+		
+		
+        <div class="sidebar-brand-text mx-3" >Mes projets</div>
+              <!-- Dropdown - User Information -->
+		
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-plus"></i>
+          <span>Nouveau Projet</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Interface
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-cog"></i>
+          <span>Components</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <a class="collapse-item" href="buttons.html">Buttons</a>
+            <a class="collapse-item" href="cards.html">Cards</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-wrench"></i>
+          <span>Utilities</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Utilities:</h6>
+            <a class="collapse-item" href="utilities-color.html">Colors</a>
+            <a class="collapse-item" href="utilities-border.html">Borders</a>
+            <a class="collapse-item" href="utilities-animation.html">Animations</a>
+            <a class="collapse-item" href="utilities-other.html">Other</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Addons
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Pages</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Login Screens:</h6>
+            <a class="collapse-item" href="login.html">Login</a>
+            <a class="collapse-item" href="register.html">Register</a>
+            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Other Pages:</h6>
+            <a class="collapse-item" href="404.html">404 Page</a>
+            <a class="collapse-item" href="blank.html">Blank Page</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Charts</span></a>
+      </li>
+
+      <!-- Nav Item - Tables -->
+      <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Tables</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            
+      <h1 class="font-weight-light">Symfonia</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Télécharger le projet en pdf</a>
+          </div>
+
+          <!-- Content Row -->
+        
+          <div class="row">
+
+                        <div class="profile-head">
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="histoire-tab" data-toggle="tab" href="#histoire" role="tab" aria-controls="histoire" aria-selected="true">Histoire</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="Personnages-tab" data-toggle="tab" href="#Personnages" role="tab" aria-controls="Personnages" aria-selected="false">Personnages</a>
+                                </li>
+								 <li class="nav-item">
+                                    <a class="nav-link" id="Bestiaire-tab" data-toggle="tab" href="#Bestiaire" role="tab" aria-controls="Bestiaire" aria-selected="false">Bestiaire</a>
+                                </li>
+								 <li class="nav-item">
+                                    <a class="nav-link" id="Lieux-tab" data-toggle="tab" href="#Lieux" role="tab" aria-controls="Lieux" aria-selected="false">Lieux et villes</a>
+                                </li>
+								 <li class="nav-item">
+                                    <a class="nav-link" id="Termes-tab" data-toggle="tab" href="#Termes" role="tab" aria-controls="Termes" aria-selected="false">Termes théoriques</a>
+                                </li>
+								 <li class="nav-item">
+                                    <a class="nav-link" id="Illustrations-tab" data-toggle="tab" href="#Illustrations" role="tab" aria-controls="Illustrations" aria-selected="false">Illustrations</a>
+                                </li>
+                            </ul>
+                        </div>
+							
+                    <div class="col-md-12">
+                        <div class="tab-content profile-tab" id="myTabContent">
+					
+							 <div class="tab-pane fade show active" id="histoire" role="tabpanel" aria-labelledby="histoire-tab">
+							   
+							 Histoire
+                            </div>
+						
+						    <div class="tab-pane fade" id="Personnages" role="tabpanel" aria-labelledby="Personnages-tab">
+                               <br>  
+							   
+								<div class="col-lg-8 col-sm-8 mb-4">
+								  <div class="card h-100">
+									<a href="#"><img class="card-img-top" src="IMAGES/giphy1.gif" alt=""></a>
+									 <div class="card-body">
+									  <h4 class="card-title">
+										<a href="#">Creve</a>
+									  </h4>
+									</div>
+								  </div>
+								</div>
+								
+								<div class="col-lg-8 col-sm-8 mb-4">
+								  <div class="card h-100">
+									<a href="#"><img class="card-img-top" src="IMAGES/dragon-ball-z.jpg" alt=""></a>
+									 <div class="card-body">
+									  <h4 class="card-title">
+										<a href="#">Shine</a>
+									  </h4>
+									</div>
+								  </div>
+								</div>
+								
+                            </div>
+						
+                            <div class="tab-pane fade show" id="Bestiaire" role="tabpanel" aria-labelledby="Bestiaire-tab">
+								   
+							 Bestiaire
+                            </div>
+							
+							  <div class="tab-pane fade show" id="Lieux" role="tabpanel" aria-labelledby="Lieux-tab">
+								   
+							  <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
+                </div>
+                <div class="card-body">
+                  <div class="text-center">
+                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="IMAGES/giphy.gif" alt="">
+                  </div>
+                  <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
+                  <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
+                </div>
+              </div>
+                            </div>
+							
+							  <div class="tab-pane fade show" id="Termes" role="tabpanel" aria-labelledby="Termes-tab">
+								   
+							 Termes
+                            </div>
+							
+							  <div class="tab-pane fade show" id="Illustrations" role="tabpanel" aria-labelledby="Illustrations-tab">
+								   
+							 Illustrations
+                            </div>
+							
+                        
+							
+						</div>
+                       
+                    </div>
+					
+          </div>
+
+
+      </div>
+      <!-- End of Main Content -->
+
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+	  
+   
     </div>
   </div>
 </div>
