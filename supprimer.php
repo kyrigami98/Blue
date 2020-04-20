08c30e4850df3039b39704fcdd92d9c6412593f1
Kriss D. D. BASSIDI B <blacklenoire4444@gmail.com>
2020-04-20 12:11:08 +0100

    changement de dossier

--- supprimer.php
+++ supprimer.php
@@ -24,13 +24,14 @@
 		}
 		elseif($_POST['supprimer'] == "personnage")
 		{	
-			$requete = $bdd->prepare('DELETE FROM personnage WHERE id_personnage = :id');
+			
+			$requete = $bdd->prepare('DELETE FROM intervenir WHERE id_personnage = :id');
 			
 			$requete->execute(array('id' => $_POST['id']));
 			
 			$requete->closeCursor();
 			
-			$requete = $bdd->prepare('DELETE FROM intervenir WHERE id_personnage = :id');
+			$requete = $bdd->prepare('DELETE FROM personnage WHERE id_personnage = :id');
 			
 			$requete->execute(array('id' => $_POST['id']));
 			
@@ -40,13 +41,13 @@
 		}
 		elseif($_POST['supprimer'] == "creature")
 		{
-			$requete = $bdd->prepare('DELETE FROM creature WHERE id_creature = :id');
+			$requete = $bdd->prepare('DELETE FROM apparaitre WHERE id_creature = :id');
 			
 			$requete->execute(array('id' => $_POST['id']));
 			
 			$requete->closeCursor();
 			
-			$requete = $bdd->prepare('DELETE FROM apparaitre WHERE id_creature = :id');
+			$requete = $bdd->prepare('DELETE FROM creature WHERE id_creature = :id');
 			
 			$requete->execute(array('id' => $_POST['id']));
 			
@@ -56,13 +57,13 @@
 		}
 		elseif($_POST['supprimer'] == "lieu")
 		{
-			$requete = $bdd->prepare('DELETE FROM lieu WHERE id_lieu = :id');
+			$requete = $bdd->prepare('DELETE FROM visiter WHERE id_lieu = :id');
 			
 			$requete->execute(array('id' => $_POST['id']));
 			
 			$requete->closeCursor();
 			
-			$requete = $bdd->prepare('DELETE FROM visiter WHERE id_lieu = :id');
+			$requete = $bdd->prepare('DELETE FROM lieu WHERE id_lieu = :id');
 			
 			$requete->execute(array('id' => $_POST['id']));
 			
@@ -72,17 +73,18 @@
 		}
 		elseif($_POST['supprimer'] == "terme")
 		{
+			$requete = $bdd->prepare('DELETE FROM citer WHERE id_terme = :id');
+			
+			$requete->execute(array('id' => $_POST['id']));
+			
+			$requete->closeCursor();
+			
 			$requete = $bdd->prepare('DELETE FROM terme WHERE id_terme = :id');
 			
 			$requete->execute(array('id' => $_POST['id']));
 			
 			$requete->closeCursor();
 			
-			$requete = $bdd->prepare('DELETE FROM citer WHERE id_terme = :id');
-			
-			$requete->execute(array('id' => $_POST['id']));
-			
-			$requete->closeCursor();
 			
 			header('Location: atelier.php');
 		}
