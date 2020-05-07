6a0ba842efeb52d7cabb88c164dbd5bede8d66a8
Kriss D. D. BASSIDI B <blacklenoire4444@gmail.com>
2020-05-07 09:21:53 +0100

    base de donnee et js

--- INCLUSION/footer.php
+++ INCLUSION/footer.php
@@ -396,6 +396,43 @@
 	 }); */
 	//voici le script pour le champ text de la page story
 
+	function suivre()
+	{
+		document.getElementById("ne_plus_suivre").style.visibility = "visible";
+		document.getElementById("suivre").style.visibility = "hidden";
+
+		var formulaire = "suivre";
+		var id = <?php echo $_GET['id']; ?>;
+
+		$.ajax({
+			url: 'TRAITEMENT/systeme.php',
+			type: 'POST',
+			dataType: 'JSON',
+			data: {
+				formulaire: formulaire,
+				id: id
+			}
+		});
+	}
+
+	function ne_plus_suivre()
+	{
+		document.getElementById('ne_plus_suivre').style.visibility = "hidden";
+		document.getElementById('suivre').style.visibility = "visible";
+
+		var formulaire = "ne_plus_suivre";
+		var id = <?php echo $_GET['id']; ?>;
+
+		$.ajax({
+			url: 'TRAITEMENT/systeme.php',
+			type: 'POST',
+			dataType: 'JSON',
+			data: {
+				formulaire: formulaire,
+				id: id
+			}
+		});
+	}
 
 	$(function() {
 		$('[data-toggle="tooltip"]').tooltip()
